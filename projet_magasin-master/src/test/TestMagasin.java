cd286d2fd0183940a3288dd0232947613aadfc33
Elain Tristan <tristan.elain@gmail.com>
2017-09-19 14:08:09 +0200

    ajout du package de test

---
+++ projet_magasin-master/src/test/TestMagasin.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author visiteur-info16
+ *
+ */
+public class TestMagasin {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
