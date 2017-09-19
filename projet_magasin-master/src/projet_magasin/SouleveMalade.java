b96cb028e9c007d419abe721a453c9575d028ec0
Cyril Dernoncourt <cyril.dernoncourt@etu.univ-nantes.fr>
2017-09-19 13:59:04 +0200

    Ajout des articles

---
+++ projet_magasin-master/src/projet_magasin/SouleveMalade.java
@@ -0,0 +1,54 @@
+package projet_magasin;
+
+public class SouleveMalade extends Article {
+
+	private int capaciteLevage;
+	private int degrePivot;
+
+	/**
+	 * @param reference
+	 * @param marque
+	 * @param modele
+	 * @param prix
+	 * @param stock
+	 * @param capaciteLevage
+	 * @param degrePrivot
+	 */
+	public SouleveMalade(String reference, String marque, String modele, double prix, int stock, int capaciteLevage,
+			int degrePivot) {
+		super(reference, marque, modele, prix, stock);
+		this.capaciteLevage = capaciteLevage;
+		this.degrePivot = degrePivot;
+	}
+
+	/**
+	 * @return the capaciteLevage
+	 */
+	public int getCapaciteLevage() {
+		return capaciteLevage;
+	}
+
+	/**
+	 * @param capaciteLevage
+	 *            the capaciteLevage to set
+	 */
+	public void setCapaciteLevage(int capaciteLevage) {
+		this.capaciteLevage = capaciteLevage;
+	}
+
+	/**
+	 * @return the degrePivot
+	 */
+	public int getDegrePivot() {
+		return degrePivot;
+	}
+
+	/**
+	 * @param degrePivot
+	 *            the degrePivot to set
+	 */
+	public void setDegrePivot(int degrePivot) {
+		this.degrePivot = degrePivot;
+	}
+
+}
